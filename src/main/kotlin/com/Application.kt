8b8f6c0d8aef3e23c43e357a268a4fc055668780
arsoft <ar.soft1245@gmail.com>
2023-11-06 17:48:04 +0500

    updated

--- src/main/kotlin/com/Application.kt
+++ src/main/kotlin/com/Application.kt
@@ -1,17 +1,13 @@
 package com
 
 import com.compose.dao.DatabaseFactory
-import com.plugins.configureRouting
 import com.plugins.configureSerialization
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
     DatabaseFactory.init()
-
     configureSerialization()
 }
